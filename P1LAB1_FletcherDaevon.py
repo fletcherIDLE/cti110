768e97ebf99dff1387f10755fe0b9178b972a4f5
fletcherIDLE <fletched2719@student.faytechcc.edu>
2024-09-22 16:51:38 -0700

    FILE:submission

---
+++ P1LAB1_FletcherDaevon.py
@@ -0,0 +1,8 @@
+# Daevon Fletcher
+# 09/22/2024
+# P1LAB1_FletcherDaevon
+# This assignment is to provide the basics of program coding using Python and IDLE.
+
+print ('Enter your first name: Daevon')
+print ('Enter your last name: Fletcher')
+print ('Hello, Daevon Fletcher! Welcome to CTI-110')
