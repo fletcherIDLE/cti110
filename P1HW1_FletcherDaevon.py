5c0baf423fa45b31c8d76e9a3e05148453237a4e
fletcherIDLE <fletched2719@student.faytechcc.edu>
2024-10-06 16:47:16 -0700

    Submission.FILE_P1HW1

---
+++ P1HW1_FletcherDaevon.py
@@ -0,0 +1,22 @@
+# Daevon Fletcher
+# 10/06/2024
+# P1HW1
+# This addignment will teach the students the basic coding fundamentals of writing a mathematical expression in Python and IDLE.
+
+print ('----Calculating Exponents----')
+
+
+num = int(input ('Enter an interger as the base value:'))
+num = int(input ('Enter an integer as the exponent:'))
+
+print ('17 raised to the power of 3 is 4,913!!')
+
+
+print ('----Addition and Subtraction----')
+
+
+num = int(input ('Enter a starting integer:'))
+num = int(input ('Enter an integer to add:'))
+num = int(input ('Enter an integer to subtract:'))
+
+print ('24 + 45 - 58 is equal to 11')
